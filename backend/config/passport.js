274309d1e05fd97a7830c3342fa7ed0ce8721ef3
Daria Malone <thefashionphysician@gmail.com>
2023-01-31 22:37:37 -0800

    working on login

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,6 +8,7 @@ module.exports = function (passport) {
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 				callbackURL: "/auth/google/callback",
+				scope: ['profile']
 			},
 			async (accessToken, refreshToken, profile, done) => {
 				const newUser = {
